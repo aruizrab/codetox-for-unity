7b23f162802d03ac1df61ebe99c1094e7f995de7
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-02-14 16:18:47 +0100

    Update MonoBehaviourExtensions.cs

--- Runtime/Core/MonoBehaviourExtensions.cs
+++ Runtime/Core/MonoBehaviourExtensions.cs
@@ -58,6 +58,12 @@ namespace Codetox.Core
         /// <summary>
         ///     Returns a vector representing the direction and distance from a point in space to this game object.
         /// </summary>
+        /// <footer>
+        ///     <a href="https://github.com/aruizrab/codetox-for-unity/wiki/MonoBehaviour-extended-functionalities#vectorfrom">
+        ///         Check the
+        ///         documentation for more
+        ///     </a>
+        /// </footer>
         public static Vector3 VectorFrom(this MonoBehaviour mono, Vector3 point)
         {
             return mono.transform.position - point;
@@ -66,6 +72,12 @@ namespace Codetox.Core
         /// <summary>
         ///     Returns a vector representing the direction and distance from a transform to this game object.
         /// </summary>
+        /// <footer>
+        ///     <a href="https://github.com/aruizrab/codetox-for-unity/wiki/MonoBehaviour-extended-functionalities#vectorfrom">
+        ///         Check the
+        ///         documentation for more
+        ///     </a>
+        /// </footer>
         public static Vector3 VectorFrom(this MonoBehaviour mono, Transform transform)
         {
             return mono.VectorFrom(transform.position);
@@ -74,22 +86,40 @@ namespace Codetox.Core
         /// <summary>
         ///     Returns a vector representing the direction and distance from a game object to this game object.
         /// </summary>
+        /// <footer>
+        ///     <a href="https://github.com/aruizrab/codetox-for-unity/wiki/MonoBehaviour-extended-functionalities#vectorfrom">
+        ///         Check the
+        ///         documentation for more
+        ///     </a>
+        /// </footer>
         public static Vector3 VectorFrom(this MonoBehaviour mono, GameObject gameObject)
         {
             return mono.VectorFrom(gameObject.transform);
         }
 
         /// <summary>
-        ///     Returns a float representing the distance between this game object and a point in space.
+        ///     Returns a normalized vector representing the direction from this game object to a point in space.
         /// </summary>
-        public static float DistanceTo(this MonoBehaviour mono, Vector3 point)
+        /// <footer>
+        ///     <a href="https://github.com/aruizrab/codetox-for-unity/wiki/MonoBehaviour-extended-functionalities#directionto">
+        ///         Check the
+        ///         documentation for more
+        ///     </a>
+        /// </footer>
+        public static Vector3 DirectionTo(this MonoBehaviour mono, Vector3 point)
         {
-            return mono.VectorTo(point).magnitude;
+            return mono.VectorTo(point).normalized;
         }
 
         /// <summary>
         ///     Returns a normalized vector representing the direction from this game object to a transform.
         /// </summary>
+        /// <footer>
+        ///     <a href="https://github.com/aruizrab/codetox-for-unity/wiki/MonoBehaviour-extended-functionalities#directionto">
+        ///         Check the
+        ///         documentation for more
+        ///     </a>
+        /// </footer>
         public static Vector3 DirectionTo(this MonoBehaviour mono, Transform transform)
         {
             return mono.DirectionTo(transform.position);
@@ -98,6 +128,12 @@ namespace Codetox.Core
         /// <summary>
         ///     Returns a normalized vector representing the direction from this game object to another game object.
         /// </summary>
+        /// <footer>
+        ///     <a href="https://github.com/aruizrab/codetox-for-unity/wiki/MonoBehaviour-extended-functionalities#directionto">
+        ///         Check the
+        ///         documentation for more
+        ///     </a>
+        /// </footer>
         public static Vector3 DirectionTo(this MonoBehaviour mono, GameObject gameObject)
         {
             return mono.DirectionTo(gameObject.transform);
@@ -106,6 +142,12 @@ namespace Codetox.Core
         /// <summary>
         ///     Returns a normalized vector representing the direction from a point in space to this game object.
         /// </summary>
+        /// <footer>
+        ///     <a href="https://github.com/aruizrab/codetox-for-unity/wiki/MonoBehaviour-extended-functionalities#directionfrom">
+        ///         Check the
+        ///         documentation for more
+        ///     </a>
+        /// </footer>
         public static Vector3 DirectionFrom(this MonoBehaviour mono, Vector3 point)
         {
             return mono.VectorFrom(point).normalized;
@@ -114,6 +156,12 @@ namespace Codetox.Core
         /// <summary>
         ///     Returns a normalized vector representing the direction from a transform to this game object.
         /// </summary>
+        /// <footer>
+        ///     <a href="https://github.com/aruizrab/codetox-for-unity/wiki/MonoBehaviour-extended-functionalities#directionfrom">
+        ///         Check the
+        ///         documentation for more
+        ///     </a>
+        /// </footer>
         public static Vector3 DirectionFrom(this MonoBehaviour mono, Transform transform)
         {
             return mono.DirectionFrom(transform.position);
@@ -122,14 +170,40 @@ namespace Codetox.Core
         /// <summary>
         ///     Returns a normalized vector representing the direction from a game object to this game object.
         /// </summary>
+        /// <footer>
+        ///     <a href="https://github.com/aruizrab/codetox-for-unity/wiki/MonoBehaviour-extended-functionalities#directionfrom">
+        ///         Check the
+        ///         documentation for more
+        ///     </a>
+        /// </footer>
         public static Vector3 DirectionFrom(this MonoBehaviour mono, GameObject gameObject)
         {
             return mono.DirectionFrom(gameObject.transform);
         }
         
         /// <summary>
+        ///     Returns a float representing the distance between this game object and a point in space.
+        /// </summary>
+        /// <footer>
+        ///     <a href="https://github.com/aruizrab/codetox-for-unity/wiki/MonoBehaviour-extended-functionalities#distanceto">
+        ///         Check the
+        ///         documentation for more
+        ///     </a>
+        /// </footer>
+        public static float DistanceTo(this MonoBehaviour mono, Vector3 point)
+        {
+            return mono.VectorTo(point).magnitude;
+        }
+
+        /// <summary>
         ///     Returns a float representing the distance between this game object and a transform.
         /// </summary>
+        /// <footer>
+        ///     <a href="https://github.com/aruizrab/codetox-for-unity/wiki/MonoBehaviour-extended-functionalities#distanceto">
+        ///         Check the
+        ///         documentation for more
+        ///     </a>
+        /// </footer>
         public static float DistanceTo(this MonoBehaviour mono, Transform transform)
         {
             return mono.DistanceTo(transform.position);
@@ -138,23 +212,27 @@ namespace Codetox.Core
         /// <summary>
         ///     Returns a float representing the distance between this game object and another game object.
         /// </summary>
+        /// <footer>
+        ///     <a href="https://github.com/aruizrab/codetox-for-unity/wiki/MonoBehaviour-extended-functionalities#distanceto">
+        ///         Check the
+        ///         documentation for more
+        ///     </a>
+        /// </footer>
         public static float DistanceTo(this MonoBehaviour mono, GameObject gameObject)
         {
             return mono.DistanceTo(gameObject.transform);
         }
-
-        /// <summary>
-        ///     Returns a normalized vector representing the direction from this game object to a point in space.
-        /// </summary>
-        public static Vector3 DirectionTo(this MonoBehaviour mono, Vector3 point)
-        {
-            return mono.VectorTo(point).normalized;
-        }
-
+        
         /// <summary>
         ///     Returns <c>true</c> if the specified <c>layer</c> is included in the specified <c>layerMask</c>, <c>false</c>
         ///     otherwise.
         /// </summary>
+        /// <footer>
+        ///     <a href="https://github.com/aruizrab/codetox-for-unity/wiki/MonoBehaviour-extended-functionalities#islayerinlayermask">
+        ///         Check the
+        ///         documentation for more
+        ///     </a>
+        /// </footer>
         public static bool IsLayerInLayerMask(this MonoBehaviour mono, int layer, LayerMask layerMask)
         {
             return layerMask == (layerMask | (1 << layer));
