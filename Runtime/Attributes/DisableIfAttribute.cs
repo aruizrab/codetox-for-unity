eb40238cd73860c0fc1d89ad8a30eafd6dd5b090
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-02-23 15:55:51 +0100

    Added DisableIf Attribute

--- Runtime/Attributes/DisableIfAttribute.cs
+++ Runtime/Attributes/DisableIfAttribute.cs
@@ -4,13 +4,13 @@ using UnityEngine;
 namespace Codetox.Attributes
 {
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
-    public class ShowIfAttribute : PropertyAttribute
+    public class DisableIfAttribute: PropertyAttribute
     {
         public readonly ComparisonType ComparisonType;
         public readonly string Name;
         public readonly object Value;
 
-        public ShowIfAttribute(string name, object value, ComparisonType comparisonType = ComparisonType.Equals)
+        public DisableIfAttribute(string name, object value, ComparisonType comparisonType = ComparisonType.Equals)
         {
             Name = name;
             Value = value;
