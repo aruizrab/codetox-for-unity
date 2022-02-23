a7f7d64f1d7089591eb6320c246ce6bab55e97c3
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-02-23 13:05:27 +0100

    Added ShowIf attribute in Codetox.Attributes namespace

---
+++ Runtime/Attributes/ShowIfAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Codetox.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field, AllowMultiple = true)]
+    public class ShowIfAttribute : PropertyAttribute
+    {
+        public readonly ComparisonType ComparisonType;
+        public readonly string Name;
+        public readonly object Value;
+
+        public ShowIfAttribute(string name, object value, ComparisonType comparisonType = ComparisonType.Equals)
+        {
+            Name = name;
+            Value = value;
+            ComparisonType = comparisonType;
+        }
+    }
+}
