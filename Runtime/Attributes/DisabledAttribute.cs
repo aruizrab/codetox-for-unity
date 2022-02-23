1e650100a1edf628ba54cf17a0111e062b1d894b
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-02-23 10:30:39 +0100

    Added Attribute Usage to Disabled Attribute in Codetox.Attributes namespace

--- Runtime/Attributes/DisabledAttribute.cs
+++ Runtime/Attributes/DisabledAttribute.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace Codetox.Attributes
 {
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
     public class DisabledAttribute : PropertyAttribute
     {
     }
