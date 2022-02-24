9bc54833f0c5866ae2a391230079b96211a7c826
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-02-24 12:40:05 +0100

    Added HideIf Attribute

---
+++ Runtime/Attributes/HideIfAttribute.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Codetox.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class HideIfAttribute : PropertyAttribute
+    {
+        public readonly ComparisonType ComparisonType;
+        public readonly string Name;
+        public readonly object Value;
+
+        public HideIfAttribute(string name, object value, ComparisonType comparisonType = ComparisonType.Equals)
+        {
+            Name = name;
+            Value = value;
+            ComparisonType = comparisonType;
+        }
+    }
+}
