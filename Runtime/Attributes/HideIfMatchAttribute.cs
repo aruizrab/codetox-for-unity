1637b114a17ac2674bbc7b5c6f55a368908f0743
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-02-25 10:11:31 +0100

    Documented HideIfMatchAttribute.cs

--- Runtime/Attributes/HideIfMatchAttribute.cs
+++ Runtime/Attributes/HideIfMatchAttribute.cs
@@ -3,12 +3,15 @@ using UnityEngine;
 
 namespace Codetox.Attributes
 {
+    /// <summary>
+    /// Hides property if the specified field matches any of the given values.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
     public class HideIfMatchAttribute : PropertyAttribute
     {
         public readonly string Name;
         public readonly object[] Values;
-
+        
         public HideIfMatchAttribute(string name, params object[] values)
         {
             Name = name;
