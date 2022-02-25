9010affc1bcbe79e6f43388cae605d3480199511
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-02-25 10:12:54 +0100

    Added documentation for DisabledAttribute.cs

--- Runtime/Attributes/DisabledAttribute.cs
+++ Runtime/Attributes/DisabledAttribute.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 namespace Codetox.Attributes
 {
+    /// <summary>
+    /// Disables property, making it visible in the inspector but not editable.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
     public class DisabledAttribute : PropertyAttribute
     {
