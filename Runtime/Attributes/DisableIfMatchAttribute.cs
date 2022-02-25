140aad4311a44b69dddf2bd131a001e71a96ad9b
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-02-25 10:39:35 +0100

    Added DisableIfMatch attribute

---
+++ Runtime/Attributes/DisableIfMatchAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace Codetox.Attributes
+{
+    /// <summary>
+    ///     Disables property if the value of the specified field matches any of the given values.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class DisableIfMatchAttribute : PropertyAttribute
+    {
+        public readonly string Name;
+        public readonly object[] Values;
+
+        public DisableIfMatchAttribute(string name, params object[] values)
+        {
+            Name = name;
+            Values = values;
+        }
+    }
+}
