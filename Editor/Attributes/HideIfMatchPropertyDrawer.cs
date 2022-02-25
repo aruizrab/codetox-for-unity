8183a4fc9aa0abdf388855d574cda747c7eb4376
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-02-25 10:07:23 +0100

    Added HideIfMatch Attribute

---
+++ Editor/Attributes/HideIfMatchPropertyDrawer.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Linq;
+using Codetox.Attributes;
+using UnityEditor;
+using UnityEngine;
+
+namespace Codetox.Editor.Attributes
+{
+    [CustomPropertyDrawer(typeof(HideIfMatchAttribute))]
+    public class HideIfMatchPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property,
+            GUIContent label)
+        {
+            if (Hide(property)) return;
+            EditorGUI.PropertyField(position, property, label);
+        }
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            return Hide(property) ? 0f : base.GetPropertyHeight(property, label);
+        }
+
+        private bool Hide(SerializedProperty property)
+        {
+            var hideIfMatch = (HideIfMatchAttribute) attribute;
+            var path = property.propertyPath.Contains(".")
+                ? Path.ChangeExtension(property.propertyPath, hideIfMatch.Name)
+                : hideIfMatch.Name;
+            var serializedProperty = property.serializedObject.FindProperty(path);
+
+            if (serializedProperty == null)
+            {
+                Debug.LogError("Cannot find property with name: " + path);
+                return true;
+            }
+
+            var targetObject = serializedProperty.serializedObject.targetObject;
+            var field = targetObject.GetType().GetField(serializedProperty.propertyPath);
+            var value = field.GetValue(targetObject);
+
+            return hideIfMatch.Values.Aggregate(false, (current, val) => current || value.Equals(val));
+        }
+    }
+}
---
+++ Runtime/Attributes/HideIfMatchAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace Codetox.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+    public class HideIfMatchAttribute : PropertyAttribute
+    {
+        public readonly string Name;
+        public readonly object[] Values;
+
+        public HideIfMatchAttribute(string name, params object[] values)
+        {
+            Name = name;
+            Values = values;
+        }
+    }
+}
