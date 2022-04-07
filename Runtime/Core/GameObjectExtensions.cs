db21c80d3be2b6520ecd08ca80815dd5872c390a
Alex Ruiz <aruizr@edu.tecnocampus.cat>
2022-04-07 18:26:28 +0200

    Added: - TransformExtensions.cs - GameObjectExtensions.cs

---
+++ Runtime/Core/GameObjectExtensions.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Codetox.Core
+{
+    public static class GameObjectExtensions
+    {
+        public static Vector3 VectorTo(this GameObject gameObject, Vector3 point)
+        {
+            return point - gameObject.transform.position;
+        }
+
+        public static Vector3 VectorTo(this GameObject gameObject, [NotNull] Transform transform)
+        {
+            if (transform == null) throw new ArgumentNullException(nameof(transform));
+            return gameObject.VectorTo(transform.position);
+        }
+
+        public static Vector3 VectorTo(this GameObject gameObject, [NotNull] GameObject other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            return gameObject.VectorTo(other.transform);
+        }
+
+        public static Vector3 VectorFrom(this GameObject gameObject, Vector3 point)
+        {
+            return gameObject.transform.position - point;
+        }
+
+        public static Vector3 VectorFrom(this GameObject gameObject, [NotNull] Transform transform)
+        {
+            if (transform == null) throw new ArgumentNullException(nameof(transform));
+            return gameObject.VectorFrom(transform.position);
+        }
+
+        public static Vector3 VectorFrom(this GameObject gameObject, [NotNull] GameObject other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            return gameObject.VectorFrom(other.transform);
+        }
+
+        public static Vector3 DirectionTo(this GameObject gameObject, Vector3 point)
+        {
+            return gameObject.VectorTo(point).normalized;
+        }
+
+        public static Vector3 DirectionTo(this GameObject gameObject, [NotNull] Transform transform)
+        {
+            if (transform == null) throw new ArgumentNullException(nameof(transform));
+            return gameObject.DirectionTo(transform.position);
+        }
+
+        public static Vector3 DirectionTo(this GameObject gameObject, [NotNull] GameObject other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            return gameObject.DirectionTo(other.transform);
+        }
+
+        public static Vector3 DirectionFrom(this GameObject gameObject, Vector3 point)
+        {
+            return gameObject.VectorFrom(point).normalized;
+        }
+
+        public static Vector3 DirectionFrom(this GameObject gameObject, [NotNull] Transform transform)
+        {
+            if (transform == null) throw new ArgumentNullException(nameof(transform));
+            return gameObject.DirectionFrom(transform.position);
+        }
+
+        public static Vector3 DirectionFrom(this GameObject gameObject, [NotNull] GameObject other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            return gameObject.DirectionFrom(other.transform);
+        }
+
+        public static float DistanceTo(this GameObject gameObject, Vector3 point)
+        {
+            return gameObject.VectorTo(point).magnitude;
+        }
+
+        public static float DistanceTo(this GameObject gameObject, [NotNull] Transform transform)
+        {
+            if (transform == null) throw new ArgumentNullException(nameof(transform));
+            return gameObject.DistanceTo(transform.position);
+        }
+
+        public static float DistanceTo(this GameObject gameObject, [NotNull] GameObject other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            return gameObject.DistanceTo(other.transform);
+        }
+
+        public static bool IsInLayerMask(this GameObject gameObject, LayerMask layerMask)
+        {
+            return layerMask == (layerMask | (1 << gameObject.layer));
+        }
+
+        public static CoroutineBuilder Coroutine(this GameObject gameObject, bool destroyOnFinish = true,
+            bool cancelOnDisable = true)
+        {
+            var coroutineBuilder = gameObject.AddComponent<CoroutineBuilder>()
+                .DestroyOnFinish(destroyOnFinish)
+                .CancelOnDisable(cancelOnDisable);
+            coroutineBuilder.hideFlags = HideFlags.HideInInspector;
+            return coroutineBuilder;
+        }
+    }
+}
---
+++ Runtime/Core/TransformExtensions.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Codetox.Core
+{
+    public static class TransformExtensions
+    {
+        public static Vector3 VectorTo(this Transform transform, Vector3 point)
+        {
+            return point - transform.position;
+        }
+
+        public static Vector3 VectorTo(this Transform transform, [NotNull] Transform other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            return transform.VectorTo(other.position);
+        }
+
+        public static Vector3 VectorTo(this Transform transform, [NotNull] GameObject gameObject)
+        {
+            if (gameObject == null) throw new ArgumentNullException(nameof(gameObject));
+            return transform.VectorTo(gameObject.transform);
+        }
+
+        public static Vector3 VectorFrom(this Transform transform, Vector3 point)
+        {
+            return transform.position - point;
+        }
+
+        public static Vector3 VectorFrom(this Transform transform, [NotNull] Transform other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            return transform.VectorFrom(other.position);
+        }
+
+        public static Vector3 VectorFrom(this Transform transform, [NotNull] GameObject gameObject)
+        {
+            if (gameObject == null) throw new ArgumentNullException(nameof(gameObject));
+            return transform.VectorFrom(gameObject.transform);
+        }
+
+        public static Vector3 DirectionTo(this Transform transform, Vector3 point)
+        {
+            return transform.VectorTo(point).normalized;
+        }
+
+        public static Vector3 DirectionTo(this Transform transform, [NotNull] Transform other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            return transform.DirectionTo(other.position);
+        }
+
+        public static Vector3 DirectionTo(this Transform transform, [NotNull] GameObject gameObject)
+        {
+            if (gameObject == null) throw new ArgumentNullException(nameof(gameObject));
+            return transform.DirectionTo(gameObject.transform);
+        }
+
+        public static Vector3 DirectionFrom(this Transform transform, Vector3 point)
+        {
+            return transform.VectorFrom(point).normalized;
+        }
+
+        public static Vector3 DirectionFrom(this Transform transform, [NotNull] Transform other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            return transform.DirectionFrom(other.position);
+        }
+
+        public static Vector3 DirectionFrom(this Transform transform, [NotNull] GameObject gameObject)
+        {
+            if (gameObject == null) throw new ArgumentNullException(nameof(gameObject));
+            return transform.DirectionFrom(gameObject.transform);
+        }
+
+        public static float DistanceTo(this Transform transform, Vector3 point)
+        {
+            return transform.VectorTo(point).magnitude;
+        }
+
+        public static float DistanceTo(this Transform transform, [NotNull] Transform other)
+        {
+            if (other == null) throw new ArgumentNullException(nameof(other));
+            return transform.DistanceTo(other.position);
+        }
+
+        public static float DistanceTo(this Transform transform, [NotNull] GameObject gameObject)
+        {
+            if (gameObject == null) throw new ArgumentNullException(nameof(gameObject));
+            return transform.DistanceTo(gameObject.transform);
+        }
+
+        public static bool IsInLayerMask(this Transform transform, LayerMask layerMask)
+        {
+            return layerMask == (layerMask | (1 << transform.gameObject.layer));
+        }
+
+        public static CoroutineBuilder Coroutine(this Transform transform, bool destroyOnFinish = true,
+            bool cancelOnDisable = true)
+        {
+            var coroutineBuilder = transform.gameObject.AddComponent<CoroutineBuilder>()
+                .DestroyOnFinish(destroyOnFinish)
+                .CancelOnDisable(cancelOnDisable);
+            coroutineBuilder.hideFlags = HideFlags.HideInInspector;
+            return coroutineBuilder;
+        }
+    }
+}
